956293b4dd1ad1df8a865c94f23a57f83d801383
Franklin Henriquez <franklin.a.henriquez@gmail.com>
2025-05-12 00:15:46 +0000

    feat: clean up git

--- bash_it_custom/aliases/aws.aliases.bash
+++
@@ -1,4 +0,0 @@
-cite about-alias
-about-alias 'custom aliases'
-
-#alias aws-show-instance=aws ec2 describe-instances --region us-east-1 --filters Name=tag:Name,Values=wub* --query 'Reservations[].Instances[].[InstanceId,PublicDnsName,State.Name,InstanceType,join(`,`,Tags[?Key==`Name`].Value)]' --output table
